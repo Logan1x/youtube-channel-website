765e3a18f26702fca0812a7db8bed58827c4cef8
noob <sharmakhushal78@gmail.com>
2020-07-01 12:29:02 +0530

    final deploy

--- khushal2/settings.py
+++ khushal2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c7p5ab6__^g1=j0712n7l1m0q@+_h(%hxy)91@a3ncsrvv6lwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
